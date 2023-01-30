163faf57c5ba41be911af3f37a0d647194ff2a53
fahimsadnan99 <fahimgithub@gmail.com>
2023-01-30 17:30:33 +0600

    add

--- client/src/Main.js
+++ client/src/Main.js
@@ -21,7 +21,7 @@ const Main = () => {
   
   const handleClick = async (inputText)=>{
     dispatch(addValue({text : inputText, serverResponse : ""}))
-    axios.post("http://localhost:5000/",{text : inputText})
+    axios.post("https://mushy-cyan-kerchief.cyclic.app/",{text : inputText})
     .then(res => setResponse({text : inputText, serverResponse : res.data.bot}))
     .catch(err => console.log(err))
    
